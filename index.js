f2c288ca6d0404277c77536c086a99e03fa4bdc7
Yassinos <castroyassine0@Gmail.com>
2022-08-30 23:46:11 +0100

    First Steps

---
+++ index.js
@@ -0,0 +1,3 @@
+function signin() {
+    window.open("https://www.facebook.com/login/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjYxODk3OTc2LCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D")
+}
